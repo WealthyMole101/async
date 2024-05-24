18fac1f777ed59ffafa502f922fcb9e8eb4c1075
vbespalov <vbespalov@astralinux.ru>
2024-05-24 09:28:06 +0300

    Asynchronous implementation of the bulk project.

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "version.h"
+#include <async.h>
+
+int version() {
+    return PROJECT_VERSION_PATCH;
+}
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    auto context = connect(3);
+    vector<string> test = {"CMD1", "CMD2", "{", "CMD3", "CMD4", "}", "{", "CMD5", "CMD6", "{", "CMD7", "CMD8", "}", "CMD9", "}", "{", "CMD10", "CMD11"};
+
+    for (auto cmd: test) {
+        receive(cmd.c_str(), cmd.size(), context);
+    }
+
+    disconnect(context);
+}
